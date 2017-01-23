445141adb21c814ef8d9c03555e0fdcd3e99ac36
rajthilak <test@megam.io>
2017-01-23 19:10:23 +0530

    implement noVNC server successfully

--- vnc.js
+++ vnc.js
@@ -14,7 +14,19 @@
  ** limitations under the License.
  */
 
-var fs = require('fs'), io = require('socket.io'), net = require('net'), util = require('util'), url = require('url'), http = require('http'), path = require('path'), mime = require('mime');
+var fs = require('fs'),
+    net = require('net'),
+    util = require('util'),
+    url = require('url'),
+    http = require('http'),
+    path = require('path'),
+    mime = require('mime');
+    policyfile = require('policyfile'),
+    Buffer = require('buffer').Buffer,
+    WebSocketServer = require('ws').Server,
+    webServer = null,
+    wsServer = null,
+    web_path = null;
 
 var vnc_tenant = require('./lib/vnc_tenant.js');
 var yaml = require('./lib/vnc_config.js');
@@ -29,7 +41,7 @@ var yaml = require('./lib/vnc_config.js');
  * Create a server to listen on port 7000. TO-DO: Make port configurable in a js
  * or json file.
  */
-var express = require('express');
+/*var express = require('express');
 var app = express();
 var server = http.createServer(app)
 //var server = https.createServer(options, app)
@@ -37,6 +49,16 @@ vnc_tenant.listen(server);
 
 server.listen(yaml.config.server.port);
 console.log("VNC Server:" + yaml.version + " listening on port =" + yaml.config.server.port);
-
+*/
 /* This take the typical use-case of serving files in ./public */
-app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + '/public'));
+
+webServer = http.createServer(vnc_tenant.http_request);
+webServer.listen(yaml.config.server.port, function() {
+    wsServer = new WebSocketServer({server: webServer,
+                                    handleProtocols: vnc_tenant.selectProtocol});
+    wsServer.on('connection', vnc_tenant.listen);
+});
+
+// Attach Flash policyfile answer service
+policyfile.createServer().listen(-1, webServer);
